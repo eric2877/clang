dd8c5306980866b768abdfbf19dfa26a43bcda36
office_mac <eliot2877@163.com>
2020-12-30 17:05:24 +0800

    Initial Commit

---
+++ Clang_learning/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Clang_learning
+//
+//  Created by 刘洋 on 2020/12/30.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
