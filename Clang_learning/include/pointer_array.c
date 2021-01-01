6f87158a6725225e3b87df4de2a4e478b1f9f988
office_mac <eliot2877@163.com>
2021-01-02 02:32:16 +0800

    增加了指针数组和动态数组的用法，实现了 realloc和alloc的应用

--- Clang_learning/include/pointer_array.c
+++ Clang_learning/include/pointer_array.c
@@ -26,16 +26,76 @@ void pointer_array(void)
     {
         fgets(buf, BUF_LEN , stdin);
         if(buf[0] == '\n') break;
+        // if input string is larger than pStr, then resize pStr loc.
         if(strnlen(pStr, str_size) + strnlen(buf, BUF_LEN) + 1 > str_size)
         {
             str_size += 100;
             pStr = realloc(pStr, str_size);
         }
-        if(strncat(pStr, buf, strnlen(buf, BUF_LEN)))
+        if(!(strncat(pStr, buf, strnlen(buf, BUF_LEN))))
         {
             printf("Something's wrong. String concatenation failed.\n");
             return;
         }
     }
+    
+    //Find and list all the words in the prose
+    size_t maxWords = 10;
+    int word_count = 0;
+    size_t word_length = 0;
+    char** pWords = calloc(maxWords,sizeof(char*));
+    int*   pnWord = calloc(maxWords, sizeof(int));
+    
+    char* ptr = NULL;
+    char* pWord = strtok_r(pStr, delimiters, &ptr);
+    bool new_word;
+    while(pWord)
+    {
+        new_word = true;
+        // Check for existing word
+        for(int i = 0 ; i< word_count + 1; ++i)
+        {
+            if( *(pWords + i) != NULL && strcmp(pWord, pWords[i]) == 0)
+            {
+                ++(pnWord[i]);
+                new_word = false;
+                break;
+            }
+        }
+        
+        if(new_word)
+        {
+            if( word_count == maxWords)
+            {
+                maxWords += WORDS_INCR;
+                pWords = realloc(pWords, maxWords * sizeof(char*));
+            }
+            word_length = (ptr - pWord)*sizeof(char*);
+            // 复制string到新分配的内存中
+//            *(pWords + word_count) = calloc(word_length, sizeof(char));
+//            strcpy(*(pWords + word_count), pWord);
+            *(pWords + word_count) = pWord;
+            *(pnWord + word_count++) = 1;
+        }
+        
+        pWord = strtok_r(NULL, delimiters, &ptr);
+    }
+    
+    //output the words and counts
+    for(int i = 0; i < word_count; ++i)
+    {
+        printf(" %-13s %3d", pWords[i], pnWord[i] );
+        if((i+1)%4==0) printf("\n");
+    }
+    
+    printf("\n");
+    free(pWords);
+    free(pnWord);
+    free(pStr);
+    pWords = NULL;
+    pStr = NULL;
+    pnWord = NULL;
+    
+    
 }
 
--- Clang_learning/main.c
+++ Clang_learning/main.c
@@ -8,7 +8,8 @@ int main(int argc, char const *argv[])
 //    Pointer_Unit();
 //    str_search();
     // 动态分配内存
-    pointer_realloc();
+//    pointer_realloc();
+    pointer_array();
     return 0;
 }
 
